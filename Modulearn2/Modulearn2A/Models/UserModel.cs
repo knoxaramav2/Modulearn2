237ce5fc22035bdf3176efa97cfc5c5ad5da32d6
knoxaramav2 <wtscott@iastate.edu>
2021-04-29 18:41:47 -0500

    Moving to MVC, EF

---
+++ Modulearn2/Modulearn2A/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Modulearn2A.Models
+{
+    public class UserModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+    }
+}
+
