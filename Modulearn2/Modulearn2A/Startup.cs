d42061ea8c10c712024d8ba19d450954596ea5f3
knoxaramav2 <wtscott@iastate.edu>
2021-05-09 14:22:14 -0500

    Toss

--- Modulearn2/Modulearn2A/Startup.cs
+++ Modulearn2/Modulearn2A/Startup.cs
@@ -1,19 +1,22 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using System.Collections.Immutable;
+using System.Collections.Concurrent;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.IdentityModel.Tokens.Jwt;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore;
-using Modulearn2A.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
+
+using Modulearn2A.Utility;
 
 namespace Modulearn2A
 {
@@ -88,16 +91,6 @@ namespace Modulearn2A
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
-    }
-
-    public class JwtConfig
-    {
-        public int AccessTokenLifetime;
-        public int RefreshTokenLifetime;
-
-        public string Secret;
-        public string Issuer;
-        public string Audience;
     }
 
     
---
+++ Modulearn2/Modulearn2A/Utility/JwtManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulearn2A.Utility
+{
+
+    public class JwtConfig
+    {
+        public int AccessTokenLifetime;
+        public int RefreshTokenLifetime;
+
+        public string Secret;
+        public string Issuer;
+        public string Audience;
+    }
+
+    public interface IJwtAuthManager
+    {
+
+    }
+
+    public class RefreshToken
+    {
+
+    }
+
+    public class JwtAuthResult
+    {
+
+    }
+
+    public class JwtAuthManager : IJwtAuthManager
+    {
+        public IImmutableDictionary<string, RefreshToken> RefreshTokens;
+
+        private readonly ConcurrentDictionary<string, RefreshToken> _refreshTokens;
+        private readonly JwtConfig _jwtConfig;
+        private readonly byte[] _secret;
+
+        public JwtAuthManager(JwtConfig jwtConfig)
+        {
+            _jwtConfig = jwtConfig;
+            _refreshTokens = new ConcurrentDictionary<string, RefreshToken>();
+            _secret = Encoding.ASCII.GetBytes(_jwtConfig.Secret);
+        }
+
+        public JwtAuthResult GenerateTokens(string userName, Claim[] claims, DateTime time)
+        {
+            var addAudienceClaim = string.IsNullOrWhiteSpace(claims?.FirstOrDefault(x =>
+                x.Type == 
+                ));
+
+            return null;
+        }
+    }
+}
